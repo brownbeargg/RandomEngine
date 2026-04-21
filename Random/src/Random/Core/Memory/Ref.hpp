39a60f01ff092f55007ac9f98fac22e35e5f62ee
binarybear <mikailoudem@gmail.com>
2026-04-21 15:04:17 +0200

    removed bug of shared ownership

--- Random/src/Random/Core/Memory/Ref.hpp
+++ Random/src/Random/Core/Memory/Ref.hpp
@@ -1,37 +1,9 @@
 #pragma once
 
+#include "Random/Core/Memory/RefCount.hpp"
+
 namespace Rand
 {
-    class RefCount;
-
-    template <typename T>
-        requires std::derived_from<T, RefCount>
-    class Ref;
-
-    class RefCount
-    {
-      public:
-        RefCount() = default;
-        RefCount(const RefCount& other);
-        RefCount(RefCount&& rhs) noexcept;
-        RefCount& operator=(const RefCount& other);
-        RefCount& operator=(RefCount&& rhs) noexcept;
-        virtual ~RefCount() = default;
-
-        uint32_t getRefCount() const { return m_RefCounted; }
-
-      private:
-        void incRefCount() const { ++m_RefCounted; }
-        void decRefCount() const { --m_RefCounted; }
-
-      private:
-        mutable std::atomic<uint32_t> m_RefCounted{};
-
-        template <typename T>
-            requires std::derived_from<T, RefCount>
-        friend class Ref;
-    };
-
     template <typename T>
         requires std::derived_from<T, RefCount>
     class Ref
@@ -41,12 +13,10 @@ namespace Rand
         Ref(T* obj);
         Ref(const Ref& other) { reset(other.m_Ref); }
         Ref(Ref&& rhs) noexcept { move(std::move(rhs)); }
-        Ref(const std::shared_ptr<T>& shared_ptr) { return reset(*shared_ptr); }
         Ref& operator=(const Ref& other) { return reset(other.m_Ref); }
         Ref& operator=(Ref&& rhs) noexcept { return move(rhs); };
         Ref& operator=(const T* const obj) { return reset(obj); }
         Ref& operator=(T* obj) { return reset(obj); }
-        Ref& operator=(const std::shared_ptr<T>& shared_ptr) { return reset(*shared_ptr); }
         ~Ref() { destroy(); }
 
         const std::optional<T&> operator*() const { return dereference(); }
@@ -55,7 +25,6 @@ namespace Rand
         T* operator->() { return get(); }
 
         Ref& reset(T* obj);
-        Ref& reset(const std::shared_ptr<T>& shared_ptr);
 
         /**
          * @brief Removes the current reference and checks if it should be deleted
@@ -90,19 +59,6 @@ namespace Rand
         destroy();
 
         m_Ref = obj;
-        if (m_Ref)
-            m_Ref->incRefCount();
-
-        return *this;
-    }
-
-    template <typename T>
-        requires std::derived_from<T, RefCount>
-    Ref<T>& Ref<T>::reset(const std::shared_ptr<T>& shared_ptr)
-    {
-        destroy();
-
-        m_Ref = new T(*shared_ptr);
         if (m_Ref)
             m_Ref->incRefCount();
 
---
+++ Random/src/Random/Core/Memory/RefCount.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace Rand
+{
+    class RefCount
+    {
+      public:
+        RefCount() = default;
+        virtual ~RefCount() = default;
+        RefCount(const RefCount& other) { m_RefCounted.store(other.m_RefCounted); }
+        RefCount(RefCount&& rhs) noexcept { m_RefCounted.store(std::move(rhs.m_RefCounted)); }
+
+        RefCount& operator=(const RefCount& other)
+        {
+            m_RefCounted.store(other.m_RefCounted);
+            return *this;
+        }
+
+        RefCount& operator=(RefCount&& rhs) noexcept
+        {
+            if (this == &rhs)
+                return *this;
+
+            m_RefCounted.store(std::move(rhs.m_RefCounted));
+            return *this;
+        }
+
+        uint32_t getRefCount() const { return m_RefCounted; }
+
+      private:
+        void incRefCount() const { ++m_RefCounted; }
+        void decRefCount() const { --m_RefCounted; }
+
+      private:
+        mutable std::atomic<uint32_t> m_RefCounted{};
+
+        template <typename T>
+            requires std::derived_from<T, RefCount>
+        friend class Ref;
+    };
+} // namespace Rand
